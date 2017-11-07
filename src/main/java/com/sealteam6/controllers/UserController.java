6bce56ea43263b1844c87ed79a0ed74ffb4b9c8e
BenDHillier <bendhillier@gmail.com>
2017-11-06 22:52:57 -0800

    Fix small bug causing incorrect url mappings

--- src/main/java/com/sealteam6/controllers/UserController.java
+++ src/main/java/com/sealteam6/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.sealteam6.controllers;
 import com.sealteam6.domainmodel.User;
 import com.sealteam6.repository.UserDetailsRepository;
 import com.sealteam6.repository.UserRepository;
+import com.sealteam6.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -17,17 +18,17 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Autowired
     private UserDetailsRepository userDetailsRepository;
 
-    @RequestMapping(name = "/findAll", method = RequestMethod.GET)
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public List<User> getAllUsers() {
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
-    @RequestMapping(name = "/details")
+    @RequestMapping(value = "/details", method = RequestMethod.GET)
     public List<UserDetails> details() {
         return userDetailsRepository.findAll();
     }
